27695c0e3a5201e53d793d836a86bfd18307b6d7
kazuki <kazuki247717@icloud.com>
2021-03-03 21:11:08 +0900

    index

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module NbaplayerApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.generators.template_engine = :slim
 
     # Configuration for the application, engines, and railties goes here.
     #
